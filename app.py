a422773b1c073bd51ecd0ed8a9b6e2c1d5067c70
Praveena <praveenay0608@gmail.com>
2026-06-08 14:35:07 +0530

    Adding Admin page commit

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ url="https://wlgqfclaytwuwqbxtcgt.supabase.co"
 key="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndsZ3FmY2xheXR3dXdxYnh0Y2d0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA1NDY1MTgsImV4cCI6MjA5NjEyMjUxOH0.Mpx7Hu9WmcU5dhi5tW8zX4aS8byZy0vOBK6ZFgzJNPk"
 supabase=create_client(url,key)
 page=st.sidebar.radio("Select Page",["Student","Admin"])
+#Student page
 if page=="Student":
     st.title("Exam Room Finder")
     rollno=st.text_input("Enter Roll Number")
@@ -20,12 +21,33 @@ if page=="Student":
                 st.write(f"Building Name:{data['buildingno']}")
             else:
                 st.error("Roll no not found")
-                
-                
-                
         except Exception as e:
             st.error(f"error:{e}")  
-            
-            
-            
-            
+#admin page
+elif page == "Admin":
+    st.title("Admin Upload")
+
+uploaded_file = st.file_uploader(
+    "Upload Excel File",
+    type=["xlsx"]
+)
+
+if uploaded_file is not None:
+
+    # Read Excel file
+    df = pd.read_excel(uploaded_file)
+
+    # Show uploaded filename
+    st.write(f"📄 {uploaded_file.name}")
+
+    # Preview heading
+    st.subheader("Preview")
+
+    # Display table
+    st.dataframe(df)
+
+    # Upload button
+    if st.button("Upload Data"):
+        st.success(
+            f"{len(df)} records uploaded successfully!"
+        )
